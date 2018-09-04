cf9191b5c73418d07c2a56670f14ca85960a809e
Pan Chao <panchao25@gmail.com>
2018-09-04 16:40:40 -0400

    Updated

--- zinnia_tinymce/static/zinnia_tinymce/js/tinymce_textareas.js
+++ zinnia_tinymce/static/zinnia_tinymce/js/tinymce_textareas.js
@@ -2,12 +2,13 @@
 * @Author: panc25
 * @Date:   2018-09-04 15:52:11
 * @Last Modified by:   panc25
-* @Last Modified time: 2018-09-04 16:15:44
+* @Last Modified time: 2018-09-04 16:32:34
 */
 
 tinyMCE.init({
-    selector: "textarea",
-    plugins: "contextmenu,directionality,fullscreen,paste,preview,searchreplace,spellchecker,visualchars,wordcount",
-    paste_auto_cleanup_on_paste : true,
-    file_browser_callback : "mce_filebrowser"
+    selector: 'textarea',
+    plugins: 'advlist,autolink,link,image,lists,charmap,print,preview,wordcount'
+    // plugins: "contextmenu,directionality,fullscreen,paste,preview,searchreplace,spellchecker,visualchars,wordcount",
+    // paste_auto_cleanup_on_paste : true,
+    // file_browser_callback : "mce_filebrowser"
 });
