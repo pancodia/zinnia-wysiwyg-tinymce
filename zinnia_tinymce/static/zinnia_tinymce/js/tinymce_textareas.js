5de41287043b7eeece40668630cac559eb078cd1
Pan Chao <panchao25@gmail.com>
2018-09-04 22:02:43 -0400

    Updated the configuration js to make zinnia-tinycme to work with django-tinymce-lite.

--- zinnia_tinymce/static/zinnia_tinymce/js/tinymce_textareas.js
+++ zinnia_tinymce/static/zinnia_tinymce/js/tinymce_textareas.js
@@ -2,12 +2,21 @@
 * @Author: panc25
 * @Date:   2018-09-04 15:52:11
 * @Last Modified by:   panc25
-* @Last Modified time: 2018-09-04 16:32:34
+* @Last Modified time: 2018-09-04 21:57:36
 */
 
 tinyMCE.init({
     selector: 'textarea',
-    plugins: 'advlist,autolink,link,image,lists,charmap,print,preview,wordcount'
+    theme: 'modern',
+    plugins: 'link image preview codesample contextmenu table code lists wordcount',
+    toolbar1: 'formatselect | bold italic underline | alignleft aligncenter alignright alignjustify \
+               | bullist numlist | outdent indent | table | link image | codesample | preview code',
+    contextmenu: 'formats | link image',
+    menubar: false,
+    inline: false,
+    statusbar: true,
+    width: 'auto',
+    height: 300,
     // plugins: "contextmenu,directionality,fullscreen,paste,preview,searchreplace,spellchecker,visualchars,wordcount",
     // paste_auto_cleanup_on_paste : true,
     // file_browser_callback : "mce_filebrowser"
